259320e381e23cac0043eb298474ff9cbbe86473
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2022-06-07 15:26:44 -0300

    Manipulando entidades com PanacheEntity

--- src/main/java/br/com/palota/quarkussocial/domain/model/User.java
+++ src/main/java/br/com/palota/quarkussocial/domain/model/User.java
@@ -1,10 +1,12 @@
 package br.com.palota.quarkussocial.domain.model;
 
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User extends PanacheEntityBase {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
