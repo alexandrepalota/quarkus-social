2c75a71b98062565a3e0c1f498e237471062140b
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2022-06-13 15:08:37 -0300

    Listando postagens

--- src/main/java/br/com/palota/quarkussocial/rest/PostResource.java
+++ src/main/java/br/com/palota/quarkussocial/rest/PostResource.java
@@ -5,12 +5,17 @@ import br.com.palota.quarkussocial.domain.model.User;
 import br.com.palota.quarkussocial.domain.repository.PostRepository;
 import br.com.palota.quarkussocial.domain.repository.UserRepository;
 import br.com.palota.quarkussocial.rest.dto.CreatePostRequest;
+import br.com.palota.quarkussocial.rest.dto.PostResponse;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import io.quarkus.panache.common.Sort;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Path("/users/{userId}/posts")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -42,6 +47,9 @@ public class PostResource {
     public Response listPost(@PathParam("userId") Long userId) {
         User user = userRepository.findById(userId);
         if (user == null) return Response.status(Response.Status.NOT_FOUND).build();
-        return Response.ok().build();
+        PanacheQuery<Post> query = postRepository.find("user", Sort.by("dateTime", Sort.Direction.Descending), user);
+        List<Post> list = query.list();
+        List<PostResponse> response = list.stream().map(PostResponse::fromEntity).collect(Collectors.toList());
+        return Response.ok(response).build();
     }
 }
---
+++ src/main/java/br/com/palota/quarkussocial/rest/dto/PostResponse.java
@@ -0,0 +1,21 @@
+package br.com.palota.quarkussocial.rest.dto;
+
+import br.com.palota.quarkussocial.domain.model.Post;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class PostResponse {
+
+    private String text;
+    private LocalDateTime dateTime;
+
+    public static PostResponse fromEntity(Post post) {
+        PostResponse response = new PostResponse();
+        response.setText(post.getText());
+        response.setDateTime(post.getDateTime());
+        return response;
+    }
+
+}
